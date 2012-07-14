903f0a5699e6f38ed5abd1b67d872e1ed66521c4
Holden Karau <holden@pigscanfly.ca>
2012-07-14 04:41:17 -0700

    Fix the tests to reset global state

--- src/simulatetests.scm
+++ src/simulatetests.scm
@@ -26,10 +26,17 @@
 
 
 
- (define (foo x) (let* 
+ (define (foo x)
+   ;;fuckit
+   (set! *best-node-so-far* #f)
+   (set! *best-node* #f)
+   ;;i've got a time machine
+   (let* 
  		((world (file->world x))
 		 (board (world-board world))
-		 (the-world (best-move-random (file->world x) (+ 100 (* (board-height board) (board-width board))) 3)))
+		 (the-world (best-move-random (file->world x) (+ 100 (* 3 (board-height board) (board-width board))) 3)))
+;;		(display (output-moves (vector-ref the-world 2)))
+;;		(display (vector-ref the-world 1))
  	      (score-world (count-hugs (file->world x)) (vector-ref the-world 2)  (vector-ref the-world 1))))
 
 ;;(test-group "flood4" 
