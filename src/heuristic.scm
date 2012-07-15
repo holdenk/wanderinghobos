774221a1bdf32fe234acb07a6873762261b9b477
Holden Karau <holden@pigscanfly.ca>
2012-07-14 21:35:37 -0700

    fuckit

--- src/heuristic.scm
+++ src/heuristic.scm
@@ -113,8 +113,8 @@ cost
 
 ;;Currently we use fairly simple
 ;;(define heuristic-world fairly-simple-heuristic-world)
-;;(define heuristic-world fairly-simple-heuristic-world)
-(define heuristic-world fairly-simple-hobofloydwarshall-world)
+(define heuristic-world fairly-simple-heuristic-world)
+;;(define heuristic-world fairly-simple-hobofloydwarshall-world)
 
 (define (score-world initialhugs path world)
   (score-world-with-min-hug-value initialhugs path world 25)
