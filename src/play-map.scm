e1129a0712c6688bcc5b2b66b36fb70ac75846b2
Michael Gregson <mgregson@qitaru.net>
2012-07-15 14:17:07 -0400

    Untabify

--- src/play-map.scm
+++ src/play-map.scm
@@ -1,3 +1,4 @@
+;; -*- indent-tabs-mode: nil -*- ;;
 (declare (uses parse-input))
 (declare (uses search))
 (declare (uses heuristic))
@@ -11,39 +12,38 @@
        (themoves (reverse (vector-ref (fuckerquest-test  w (+ 250 (* 3 (*  wwidth))) 3) 2))))
   (display w)
   (foldl (lambda (s m)
-	   ;;ouput the list of murh costs
-	   (map (lambda (possible-move)
-		  (let* ((post-move (move-robot s possible-move))
-			 (newworld (if (eq? #f post-move)
-				       #f
-				       (simulate (move-robot s possible-move)))))
-		    (display possible-move) 
-		    (display ":")
-		    ;;Hack doesn't look @ path so far right now but whatever
-		    (if (eq? #f newworld)
-			(display "not valid")
-			(display (heuristic-world initialhugs (list ) newworld)))
-		    (display "\n")))
-		moves)
-	   
-	   (cond
-	    ((eq? m 'wait)
-	     (let ((r (simulate s)))
-	       (display r)
-	       r))
-	    ((eq? m 'abort)
-	     (display s)
-	     s)
-	    (else
-	     (let ((r (simulate (move-robot s m))))
-	       (display "i-am-dead?: ")
-	       (display (i-am-dead? r))
-	       (display "\n")
-	       (display r)
-	       r))))
-	 w
-	 themoves)
+           ;;ouput the list of murh costs
+           (map (lambda (possible-move)
+                  (let* ((post-move (move-robot s possible-move))
+                         (newworld (if (eq? #f post-move)
+                                       #f
+                                       (simulate (move-robot s possible-move)))))
+                    (display possible-move) 
+                    (display ":")
+                    ;;Hack doesn't look @ path so far right now but whatever
+                    (if (eq? #f newworld)
+                        (display "not valid")
+                        (display (heuristic-world initialhugs (list ) newworld)))
+                    (display "\n")))
+                moves)
+           
+           (cond
+            ((eq? m 'wait)
+             (let ((r (simulate s)))
+               (display r)
+               r))
+            ((eq? m 'abort)
+             (display s)
+             s)
+            (else
+             (let ((r (simulate (move-robot s m))))
+               (display "i-am-dead?: ")
+               (display (i-am-dead? r))
+               (display "\n")
+               (display r)
+               r))))
+         w
+         themoves)
   (display "\n")
-  (display (output-moves (reverse themoves)))
-)
+  (display (output-moves (reverse themoves))))
 (display "\n")
