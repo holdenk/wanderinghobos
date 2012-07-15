5127c6f3e589fa0a9943f562501e8bae90bb3863
Michael Gregson <mgregson@qitaru.net>
2012-07-15 14:18:01 -0400

    Untabify

--- src/main.scm
+++ src/main.scm
@@ -1,3 +1,4 @@
+;; -*- indent-tabs-mode: nil -*- ;;
 (declare (uses parse-input simulate heuristic search failsafe))
 (use sequences vector-lib posix)
 ;;Setup the failsaif
@@ -5,17 +6,17 @@
 (register-failsafe)
 ;;main business
 (let* ((world (parse-input))
-			 (wheight (vector-length (world-board world)))
-			 (wwidth (car (vector->list
-										 (vector-map (lambda (i e) (vector-length e))
-																 (world-board world)))))
-			 (moves (reverse (vector-ref 
-					  ;;If we have command line arguments run "quickly"
-					  (if (> (length (command-line-arguments)) 0)
-					  (best-move world (+ 100 (* 2 (* wheight wwidth))))
-					  ;;Otherwise run forver
-					  (fuckerquest-prod world (+ 250 (* 3 (* wheight wwidth))) +inf.0)
-					  )
-					  2))))
-	(display (output-moves moves)))
+       (wheight (vector-length (world-board world)))
+       (wwidth (car (vector->list
+                     (vector-map (lambda (i e) (vector-length e))
+                                 (world-board world)))))
+       (moves (reverse (vector-ref 
+                        ;;If we have command line arguments run "quickly"
+                        (if (> (length (command-line-arguments)) 0)
+                            (best-move world (+ 100 (* 2 (* wheight wwidth))))
+                            ;;Otherwise run forver
+                            (fuckerquest-prod world (+ 250 (* 3 (* wheight wwidth))) +inf.0)
+                            )
+                        2))))
+  (display (output-moves moves)))
 
