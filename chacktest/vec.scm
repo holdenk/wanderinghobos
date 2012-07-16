ec0bd3a90ccf30c48bc2897f97e8c21acd6a2a2b
Holden Karau <holden@pigscanfly.ca>
2012-07-15 17:45:57 -0700

    Fix vector

---
+++ chacktest/vec.scm
@@ -0,0 +1,12 @@
+(import foreign)
+(use srfi-4)
+#>
+int hobopass(int a, char * vec, int vecsize) {
+		      printf("called with %d e0 (%d) size %d\n",a,vec[0],vecsize);
+		      return 0;
+}
+<#
+(define a (make-s8vector 4))
+(s8vector-set! a 0 2)
+(define passtest (foreign-lambda integer "hobopass" integer s8vector integer))
+(passtest 1 a (s8vector-length a))
