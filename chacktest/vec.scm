be9472eeec7d2ee0dd5fb12ca32c1cc9a1f40e76
Holden Karau <holden@pigscanfly.ca>
2012-07-15 18:15:30 -0700

    add syed albiz

--- chacktest/vec.scm
+++ chacktest/vec.scm
@@ -1,16 +1,17 @@
 (import foreign)
 (use srfi-4)
 #>
-int hobopass(int a, char * vec, int vecsize) {
-		      printf("called with %d e0 (%d) size %d\n",a,vec[0],vecsize);
+int hobopass(int a, char * vec, int vecsize, char b) {
+		      printf("called with %d e0 (%d) size %d bool %d\n",a,vec[0],vecsize,b);
 		      vec[1] = 2;
 		      return 0;
 }
 <#
 (define a (make-s8vector 4))
 (s8vector-set! a 0 2)
-(define passtest (foreign-lambda integer "hobopass" integer s8vector integer))
-(passtest 1 a (s8vector-length a))
+(define passtest (foreign-lambda integer "hobopass" integer s8vector integer bool))
+(passtest 1 a (s8vector-length a) #f)
+(passtest 1 a (s8vector-length a) #t)
 (display "ilike:")
 (display (s8vector-ref a 1))
 (display "sexy\n")
