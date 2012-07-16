c043176f84e663011de42dfd4a036b83c7637ff9
Holden Karau <holden@pigscanfly.ca>
2012-07-16 00:21:03 -0700

    Start haxing to support simulating the world

--- src/csimulate.c
+++ src/csimulate.c
@@ -1,5 +1,58 @@
 #include "csimulate.h"
 
+#define NULL 0
+
+cworld * make_cworld(cboard* board, int water, int flooding, int waterproof,
+		     int underwater, int iteration, int robot_x,
+		     int robot_y, int lift_x, int lift_y, int fuckedrocks,
+		     int beard, int razors) {
+  cworld * w = (cworld *)malloc (sizeof(cworld));
+  w->water = water;
+  w->flooding = flooding;
+  w->waterproof = waterproof;
+  w->underwater = underwater;
+  w->iteration = iteration;
+  w->rocks = NULL;
+  w->robot_x = robot_x;
+  w->robot_y = robot_y;
+  w->hugs = NULL;
+  w->lift_x = lift_x;
+  w->lift_y = lift_y;
+  w->fuckedrocks = fuckedrocks;
+  w->beard = beard;
+  w->razors = razors;
+  w->hug_count = -1;
+  w->fallen_rock_count = -1;
+}
+
+sim_board_info * native_simulate_board(cboard *in, cboard * out, int nr_hugs, int grow_beard) {
+  int y,x;
+  memcpy(out->board,in->board,in->width*in->height*sizeof(char));
+  sim_board_info * r = malloc(sizeof(sim_board_info));
+  r->falling_rocks_count = 0;
+  r->falling_rocks = 0;
+  //TODO MAYBE: fix!
+  r->moving_rocks = 1;
+  point * pt = malloc(sizeof(point));
+  for (y = 0; y < in->height ; ++y) {
+    for (x = 0; x < in->width ; ++x) {
+      pt->x = x;
+      pt->y = y;
+      point * p = native_execute_square(in,pt,out,nr_hugs,grow_beard);
+      if (p != 0) {
+	r->falling_rocks_count++;
+	r->falling_rocks = realloc(r->falling_rocks,sizeof(int)*2*r->falling_rocks_count);
+	r->falling_rocks[2*r->falling_rocks_count-2] = p->x;
+	r->falling_rocks[2*r->falling_rocks_count-1] = p->y;
+	free(p);
+      }
+    }
+  }
+}
+void * native_simulate_world(cworld* in, cworld* out) {
+  //TODO
+}
+
 cboard * make_board(int width, int height, signed char * tehboard) {
   cboard *b = (cboard *)malloc (sizeof(cboard));
   b->width = width;
@@ -15,12 +68,12 @@ point * make_point(int x, int y) {
   return p;
 }
 
-
-extern point native_execute_square(cboard* in,
+point * real_native_execute_square(cboard* in,
 				   point* pt_in,
 				   cboard* out,
 				   int hugs,
-				   char beard)
+				   int beard,
+				   int reversedX)
 {
 	struct point result;
 	struct point pt;
@@ -30,9 +83,9 @@ extern point native_execute_square(cboard* in,
 	int dx, dy;
 	if(IS_ROCK_LIKE(xy))
 		{
-			if(IS_EMPTY(CELL_BOARDPTR(in,pt.x,pt.y-1)))
+		  if(IS_EMPTY(CELL_BOARDPTR(in,pt.x,pt.y-1)))
 				{
-					CELL_BOARDPTR(out,pt.x,pt.y) = ' ';
+				  CELL_BOARDPTR(out,pt.x,pt.y) = ' ';
 					CELL_BOARDPTR(out,pt.x,pt.y-1) = xy;
 					result.x = pt.x;
 					result.y = pt.y-1;
@@ -75,6 +128,20 @@ extern point native_execute_square(cboard* in,
 			CELL_BOARDPTR(out,pt.x,pt.x) = xy;
 		}
 }
+
+point * native_execute_square(cboard * in,
+			      point * pt_in,
+			      cboard * out,
+			      int hugs,
+			      int beard) {
+  return real_native_execute_square(in,
+				    pt_in,
+				    out,
+				    hugs,
+				    beard,
+				    0);
+}
+
 
 /*
 int simulate_board(struct cboard* b, int hugs, char beard)
--- src/csimulate.h
+++ src/csimulate.h
@@ -18,6 +18,41 @@ struct point
 
 typedef struct point point;
 
+struct sim_board_info {
+  int* falling_rocks;
+  int falling_rocks_count;
+  int moving_rocks;
+};
+
+typedef struct sim_board_info sim_board_info;
+
+struct cworld {
+  cboard* board;
+  int water;
+  int flooding;
+  int waterproof;
+  int underwater;
+  int iteration;
+  int * rocks;
+  int fallen_rock_count;
+  int robot_x;
+  int robot_y;
+  int * hugs;
+  int hug_count;
+  int lift_x;
+  int lift_y;
+  int fuckedrocks;
+  int beard;
+  int razors;
+};
+
+typedef struct cworld cworld;
+
+extern cworld * make_cworld(cboard* board, int water, int flooding, int waterproof,
+			    int underwater, int iteration, int robot_x,
+			    int robot_y, int lift_x, int lift_y, int fuckedrocks,
+			    int beard, int razors);
+
 extern point * make_point(int x, int y);
 
 #define APT(x,y,w) (y*w+x)
@@ -27,10 +62,14 @@ extern point * make_point(int x, int y);
 #define IS_HUG(a) (a == '\\')
 #define IS_CLOSED_LIFT(a) (a == 'L')
 #define IS_BEARD(a) (a == 'W')
+#define CONVERTPOINT(x,y,in) reversedX*in->height-x,y
 
-extern point native_execute_square(cboard* in,
+extern point * native_execute_square(cboard* in,
 				   point* pt,
 				   cboard* out,
 				   int hugs,
-				   char beard);
+				   int beard);
+
+extern sim_board_info * native_simulate_board(cboard *in, cboard *out, int nr_hugs, int grow_beard);
+
 #endif
